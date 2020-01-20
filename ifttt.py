4eda0816e8579954a68e16a7c51de7a047f9f6fa
xumingjing <xumingjing@corp-ci.com>
2020-01-20 14:41:58 +0800

    feat:精简代码

--- ifttt.py
+++ ifttt.py
@@ -1,5 +1,6 @@
 import requests
 
+
 def send(value1):
     event_name = 'stock'
     key = 'd52n-XLnjmsFF9fZ6AX6sB'
--- web/app.py
+++ web/app.py
@@ -1,11 +1,10 @@
 from random import randrange
-from daily_market import DailyIndex
+from daily_market import DailyIndex, DailyQuotation
 
 from flask import Flask, render_template
 
 from pyecharts import options as opts
 from pyecharts.charts import Bar
-
 
 app = Flask(__name__, static_folder="templates")
 
@@ -13,10 +12,10 @@ app = Flask(__name__, static_folder="templates")
 def bar_base() -> Bar:
     c = (
         Bar()
-        .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
-        .add_yaxis("商家A", [randrange(0, 100) for _ in range(6)])
-        .add_yaxis("商家B", [randrange(0, 100) for _ in range(6)])
-        .set_global_opts(title_opts=opts.TitleOpts(title="Bar-基本示例", subtitle="我是副标题"))
+            .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
+            .add_yaxis("商家A", [randrange(0, 100) for _ in range(6)])
+            .add_yaxis("商家B", [randrange(0, 100) for _ in range(6)])
+            .set_global_opts(title_opts=opts.TitleOpts(title="Bar-基本示例", subtitle="我是副标题"))
     )
     return c
 
